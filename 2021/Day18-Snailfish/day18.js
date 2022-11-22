97db849224406a2797f5a4e179ec1034d71befa2
Martin Anderson Graham <martin.anderson.graham@gmail.com>
2022-11-22 08:12:31 -0700

    Finished 2021 day 18

--- 2021/Day18-Snailfish/day18.js
+++ 2021/Day18-Snailfish/day18.js
@@ -1,49 +1,143 @@
+const arrayize = str => {
+  return str.split('').map(ele => {
+    if (Number.isInteger(Number(ele))) {
+      return Number(ele)
+    }
+    return ele
+  });
+}
+
+const unarrayize = arr => {
+  return JSON.parse(arr)
+}
+
 const add = (str1, str2) => {
   return `[${str1},${str2}]`
 }
 
-const explode = (str, depth) => {
+const addLineArrs = (arr1, arr2) => {
+  return ['[', ...arr1, ',', ...arr2, ']']
+}
+
+const explode = (lineArr) => {
   let depthCount = 0;
-  for (let i = 0; i < str.length; i++) {
-    let char = str.charAt(i)
+  for (let i = 0; i < lineArr.length; i++) {
+    let char = lineArr[i]
     if (char === '[') depthCount += 1
     if (char === ']') depthCount -= 1
     if (depthCount === 5) {
-      let firstNum = Number(str.charAt(i + 1))
-      let secondNumber = Number(str.charAt(i + 3))
+      let firstNum = Number(lineArr[i + 1])
+      let secondNumber = Number(lineArr[i + 3])
 
       let closingIndex = i
-      for (let c = i; c < str.length; c++) {
+      for (let c = i; c < lineArr.length; c++) {
         closingIndex = c;
-        if (str.charAt(c) === ']') break
+        if (lineArr[c] === ']') break
       };
 
-      let firstHalf = str.slice(0, i)
-      let secondHalf = str.slice(closingIndex + 1)
+      let firstHalf = lineArr.slice(0, i)
+      let secondHalf = lineArr.slice(closingIndex + 1)
 
       for (let j = 0; j < secondHalf.length; j++) {
-        if (Number(secondHalf.charAt(j))) {
-          secondHalf = secondHalf.slice(0, j) + String(Number(secondHalf.charAt(j)) + secondNumber) + secondHalf.slice(j + 1)
+        if (Number.isInteger(Number(secondHalf[j]))) {
+          secondHalf[j] += secondNumber
           break
         };
       };
 
       for (let j = firstHalf.length - 1; j > 0; j--) {
-        if (Number(firstHalf.charAt(j))) {
-          firstHalf = firstHalf.slice(0, j) + String(Number(firstHalf.charAt(j)) + firstNum) + firstHalf.slice(j + 1)
+        if (Number.isInteger(Number(firstHalf[j]))) {
+          firstHalf[j] += firstNum
           break
         };
       };
 
-
-      return [true, firstHalf + '0' + secondHalf]
+      return [true, [...firstHalf, 0, ...secondHalf]]
     };
   };
-  return [false, str]
+  return [false, lineArr]
 };
 
+const split = (lineArr) => {
+  let changed = false
+  for (let i = 0; i < lineArr.length; i++) {
+    if (Number(lineArr[i]) && Number(lineArr[i]) > 9) {
+      changed = true
+      let num = Number(lineArr[i])
+      let first = Math.floor(num / 2)
+      let second = Math.ceil(num / 2)
+      lineArr = [...lineArr.slice(0, i), '[', first, ',', second, ']', ...lineArr.slice(i + 1)]
+      break
+    };
+  };
+  return [changed, lineArr]
+};
 
+const reduce = (lineArr) => {
+  let changed = true;
+  while (changed) {
+    changed = false
+    let [didExplode, newLineArr] = explode(lineArr)
+    if (didExplode) {
+      changed = true;
+      lineArr = newLineArr
+    } else {
+      let [didSplit, snewLineArr] = split(lineArr)
+      if (didSplit) {
+        changed = true
+        lineArr = snewLineArr
+      };
+    }
+  };
+  return lineArr
+};
+
+const totalSum = (inputString) => {
+  const strArr = inputString.split('\n');
+  const lineArrs = strArr.map(ele => arrayize(ele))
+  let sum = lineArrs[0]
+  for (let i = 1; i < lineArrs.length; i++) {
+    sum = reduce(addLineArrs(sum, lineArrs[i]))
+  };
+  return sum
+}
+
+const magnitude = (arr) => {
+  if (!Array.isArray(arr)) {
+    return arr
+  }
+  return 3 * magnitude(arr[0]) + 2 * magnitude(arr[1])
+}
+
+const part1 = (inputString) => {
+  const arrSum = totalSum(inputString)
+  return magnitude(unarrayize(arrSum.join('')))
+};
+
+const part2 = (inputString) => {
+  const strArr = inputString.split('\n');
+  const lineArrs = strArr.map(ele => arrayize(ele))
+  let maxSum = -Infinity
+  for (let i = 0; i < lineArrs.length; i++) {
+    for (let j = 0; j < lineArrs.length; j++) {
+      if (i === j) continue
+      let sum = part1(lineArrs[i].join('') + '\n' + lineArrs[j].join(''))
+      if (sum > maxSum) {
+        maxSum = sum
+      };
+    };
+  };
+  return maxSum
+}
 module.exports = {
+  arrayize,
+  unarrayize,
+  add,
   explode,
-  add
+  split,
+  reduce,
+  totalSum,
+  magnitude,
+  part1,
+  part2,
 }
--- 2021/Day18-Snailfish/day18.test.js
+++ 2021/Day18-Snailfish/day18.test.js
@@ -1,33 +1,54 @@
 let {
+  sample1,
+  sample2,
+  sample3,
+  sample4,
+  sample5,
+  part1input
+} = require('./input');
+let {
+  arrayize,
+  unarrayize,
   add,
-  explode
+  explode,
+  split,
+  reduce,
+  totalSum,
+  part1,
+  part2,
+  magnitude,
 } = require('./day18')
 
 test('explode', () => {
-  let first = '[[[[[9,8],1],2],3],4]'
+  let first = arrayize('[[[[[9,8],1],2],3],4]')
   let [_fv, fr] = explode(first)
   let fe = '[[[[0,9],2],3],4]';
-  expect(fr).toBe(fe)
+  expect(fr.join('')).toBe(fe)
 
-  let second = '[7,[6,[5,[4,[3,2]]]]]'
+  let second = arrayize('[7,[6,[5,[4,[3,2]]]]]')
   let [_sv, sr] = explode(second)
   let se = '[7,[6,[5,[7,0]]]]'
-  expect(sr).toBe(se)
+  expect(sr.join('')).toBe(se)
 
-  let third = '[[6,[5,[4,[3,2]]]],1]'
+  let third = arrayize('[[6,[5,[4,[3,2]]]],1]')
   let [_tv, tr] = explode(third)
   let te = '[[6,[5,[7,0]]],3]'
-  expect(tr).toBe(te)
+  expect(tr.join('')).toBe(te)
 
-  let fourth = '[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]'
+  let fourth = arrayize('[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]')
   let [_frv, frr] = explode(fourth)
   let fre = '[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]'
-  expect(frr).toBe(fre)
+  expect(frr.join('')).toBe(fre)
 
-  let fifth = '[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]'
+  let fifth = arrayize('[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]')
   let [_ffv, ffr] = explode(fifth)
   let ffe = '[[3,[2,[8,0]]],[9,[5,[7,0]]]]'
-  expect(ffr).toBe(ffe)
+  expect(ffr.join('')).toBe(ffe)
+
+  let sixth = arrayize('[[[[0,7],4],[[7,8],[0,[6,7]]]],[1,1]]')
+  let [_sxv, sxr] = explode(sixth)
+  let sxe = '[[[[0,7],4],[[7,8],[6,0]]],[8,1]]'
+  expect(sxr.join('')).toBe(sxe)
 })
 
 test('add', () => {
@@ -37,3 +58,83 @@ test('add', () => {
   expect(result).toBe(`[[1,2],[[3,4],5]]`)
 })
 
+test("split", () => {
+  let first = [10]
+  let [_fv, fr] = split(first)
+  expect(fr.join('')).toBe('[5,5]')
+
+  let second = [11]
+  let [_sv, sr] = split(second)
+  expect(sr.join('')).toBe('[5,6]')
+
+  let third = [12]
+  let [_tv, tr] = split(third)
+  expect(tr.join('')).toBe('[6,6]')
+})
+
+test("reduce", () => {
+  let input = arrayize('[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]')
+  let result = reduce(input)
+  expect(result.join('')).toBe('[[[[0,7],4],[[7,8],[6,0]]],[8,1]]')
+
+  let two = add('[[[[4,3],4],4],[7,[[8,4],9]]]', '[1,1]')
+  let twoarr = arrayize(two)
+  let result2 = reduce(twoarr)
+  expect(result2.join('')).toBe('[[[[0,7],4],[[7,8],[6,0]]],[8,1]]')
+})
+
+test("sample multi-lines", () => {
+  let input1 = sample1
+  let result1 = totalSum(input1)
+  expect(result1.join('')).toBe('[[[[1,1],[2,2]],[3,3]],[4,4]]')
+
+  let input2 = sample2
+  let result2 = totalSum(input2)
+  expect(result2.join('')).toBe('[[[[3,0],[5,3]],[4,4]],[5,5]]')
+
+  let input3 = sample3
+  let result3 = totalSum(input3)
+  expect(result3.join('')).toBe('[[[[5,0],[7,4]],[5,5]],[6,6]]')
+
+  let input4 = sample4
+  let result4 = totalSum(input4)
+  expect(result4.join('')).toBe('[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]')
+})
+
+test("magnitude", () => {
+  let first = `[1,9]`
+  let result1 = magnitude(unarrayize(first))
+  expect(result1).toBe(21)
+
+  let second = `[9,1]`
+  let result2 = magnitude(unarrayize(second))
+  expect(result2).toBe(29)
+
+  let third = '[[9,1],[1,9]]'
+  let result3 = magnitude(unarrayize(third))
+  expect(result3).toBe(129)
+
+  let fourth = '[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]'
+  let result4 = magnitude(unarrayize(fourth))
+  expect(result4).toBe(3488)
+})
+
+test("part1", () => {
+  let input1 = sample5
+  let result1 = part1(input1)
+  expect(result1).toBe(4140)
+
+  let input2 = part1input
+  let result2 = part1(input2)
+  expect(result2).toBe(4145)
+})
+
+test("part2", () => {
+  let input1 = sample5
+  let result1 = part2(input1)
+  expect(result1).toBe(3993)
+
+  let input2 = part1input
+  let result2 = part2(input2)
+  expect(result2).toBe(4855)
+});
