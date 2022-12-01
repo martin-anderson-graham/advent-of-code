81a44c00132f7745a3c4e7b396378d5de45f5c43
Martin Anderson Graham <martin.anderson.graham@gmail.com>
2022-12-01 10:52:00 -0600

    Update 2022 day 01

--- 2022/Day01-Calorie-Counting/day01.ts
+++ 2022/Day01-Calorie-Counting/day01.ts
@@ -4,15 +4,18 @@ const parseInput = (str: string): number[][] => {
     .map((elfString) => elfString.split("\n").map((e) => Number(e)));
 };
 
+const reduceSum = (sum: number, e: number): number => sum + e;
+
 const part1 = (arr: number[][]): number => {
-  return Math.max(...arr.map((elf) => elf.reduce((sum, e) => sum + e, 0)));
+  return Math.max(...arr.map((elf) => elf.reduce(reduceSum, 0)));
 };
 
 const part2 = (arr: number[][]): number => {
   return arr
-    .map((elf) => elf.reduce((sum, e) => sum + e, 0))
+    .map((elf) => elf.reduce(reduceSum, 0))
     .sort((a, b) => b - a)
     .slice(0, 3)
-    .reduce((sum, ele) => sum + ele, 0);
+    .reduce(reduceSum, 0);
 };
+
 export { parseInput, part1, part2 };
