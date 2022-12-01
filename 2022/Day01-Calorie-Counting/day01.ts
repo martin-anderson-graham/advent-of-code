543c5ac57795db6ae54346e4a1a0c1500f0ecf8c
Martin Anderson Graham <martin.anderson.graham@gmail.com>
2022-12-01 10:04:42 -0600

    Update 2022 day 01

--- 2022/Day01-Calorie-Counting/day01.ts
+++ 2022/Day01-Calorie-Counting/day01.ts
@@ -1,41 +1,18 @@
 const parseInput = (str: string): number[][] => {
-  return str.split("\n").reduce(
-    (r: number[][], ele) => {
-      if (ele === "") {
-        r.push([]);
-      } else {
-        r[r.length - 1].push(Number(ele));
-      }
-      return r;
-    },
-    [[]]
-  );
+  return str
+    .split("\n\n")
+    .map((elfString) => elfString.split("\n").map((e) => Number(e)));
 };
 
 const part1 = (arr: number[][]): number => {
-  return Math.max(
-    ...arr.map((elf) => {
-      return elf.reduce((sum, e) => {
-        sum += e;
-        return sum;
-      }, 0);
-    })
-  );
+  return Math.max(...arr.map((elf) => elf.reduce((sum, e) => sum + e, 0)));
 };
 
 const part2 = (arr: number[][]): number => {
   return arr
-    .map((elf) => {
-      return elf.reduce((sum, e) => {
-        sum += e;
-        return sum;
-      }, 0);
-    })
+    .map((elf) => elf.reduce((sum, e) => sum + e, 0))
     .sort((a, b) => b - a)
     .slice(0, 3)
-    .reduce((sum, ele) => {
-      sum += ele;
-      return sum;
-    }, 0);
+    .reduce((sum, ele) => sum + ele, 0);
 };
 export { parseInput, part1, part2 };
