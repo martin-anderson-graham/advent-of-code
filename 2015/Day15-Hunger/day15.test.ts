f7538b76b66a8ca1235c96410a5b1dad47589b9a
Martin Anderson Graham <martin.anderson.graham@gmail.com>
2022-12-06 22:05:50 -0600

    Finished 2015 day15

--- 2015/Day15-Hunger/day15.test.ts
+++ 2015/Day15-Hunger/day15.test.ts
@@ -1,7 +1,28 @@
 import { sample, input } from "./input";
-import { parseInput, part1 } from "./day15";
+import { parseInput, part1, part2, score1 } from "./day15";
+
+test("score", () => {
+  let i = parseInput(sample);
+  let res = score1(i, { Butterscotch: 44, Cinnamon: 56 });
+  expect(res).toBe(62842880);
+});
 
 test("sample", () => {
   let res = part1(sample, 100);
   expect(res).toBe(62842880);
+});
+
+test("part1", () => {
+  let res = part1(input, 100);
+  expect(res).toBe(13882464);
+});
+
+test("sample2", () => {
+  let res = part2(sample, 100, 500);
+  expect(res).toBe(57600000);
+});
+
+test("part2", () => {
+  let res = part2(input, 100, 500);
+  expect(res).toBe(11171160);
 });
--- 2015/Day15-Hunger/day15.ts
+++ 2015/Day15-Hunger/day15.ts
@@ -21,16 +21,131 @@ const parseInput = (str: string): Ingredient[] => {
   });
 };
 
-const score = (
+const score1 = (
   ingredients: Ingredient[],
   solution: Record<string, number>
-): number => {};
+): number => {
+  let result: Record<string, number> = {
+    capacity: 0,
+    durability: 0,
+    flavor: 0,
+    texture: 0,
+    // calories: 0,
+  };
+  Object.keys(solution).forEach((key) => {
+    let qty = solution[key];
+    let ing = ingredients.find((i) => i.name === key);
+    result.capacity += ing.capacity * qty;
+    result.durability += ing.durability * qty;
+    result.flavor += ing.flavor * qty;
+    result.texture += ing.texture * qty;
+  });
+  if (Object.values(result).some((val) => val <= 0)) {
+    return 0;
+  }
+  return Object.values(result).reduce((prod, ele) => prod * ele, 1);
+};
 
 const part1 = (str: string, limit: number): number => {
   const ingredients = parseInput(str);
   const allIngredients: string[] = ingredients.map((i) => i.name);
+  let max = -Infinity;
 
-  return 3;
+  const backtrack = (
+    solution: Record<string, number>,
+    remainingIngredients: string[],
+    total: number
+  ): void => {
+    if (remainingIngredients.length === 1) {
+      solution[remainingIngredients[0]] = limit - total;
+      let scr = score1(ingredients, solution);
+      if (scr > max) {
+        max = scr;
+      }
+      delete solution[remainingIngredients[0]];
+      return;
+    } else {
+      remainingIngredients.forEach((ing) => {
+        let newRemaining = remainingIngredients.filter((i) => i !== ing);
+        for (let i = 1; i < limit - total + 1; i++) {
+          solution[ing] = i;
+          backtrack(solution, newRemaining, total + i);
+          delete solution[ing];
+        }
+      });
+    }
+  };
+
+  backtrack({} as Record<string, number>, allIngredients, 0);
+  return max;
 };
 
-export { parseInput, part1 };
+const score2 = (
+  ingredients: Ingredient[],
+  solution: Record<string, number>,
+  calLimit: number
+): number => {
+  let result: Record<string, number> = {
+    capacity: 0,
+    durability: 0,
+    flavor: 0,
+    texture: 0,
+    calories: 0,
+  };
+  Object.keys(solution).forEach((key) => {
+    let qty = solution[key];
+    let ing = ingredients.find((i) => i.name === key);
+    result.capacity += ing.capacity * qty;
+    result.durability += ing.durability * qty;
+    result.flavor += ing.flavor * qty;
+    result.texture += ing.texture * qty;
+    result.calories += ing.calories * qty;
+  });
+  if (
+    Object.values(result).some((val) => val <= 0) ||
+    result.calories !== calLimit
+  ) {
+    return 0;
+  }
+  let res =
+    Object.values(result).reduce((prod, ele) => prod * ele, 1) /
+    result.calories;
+  // console.log(solution, result, res);
+  return res;
+};
+
+const part2 = (str: string, limit: number, calLimit: number): number => {
+  const ingredients = parseInput(str);
+  const allIngredients: string[] = ingredients.map((i) => i.name);
+  let max = -Infinity;
+
+  const backtrack = (
+    solution: Record<string, number>,
+    remainingIngredients: string[],
+    total: number
+  ): void => {
+    if (remainingIngredients.length === 1) {
+      solution[remainingIngredients[0]] = limit - total;
+      let scr = score2(ingredients, solution, calLimit);
+      if (scr > max) {
+        // console.log(solution, max);
+        max = scr;
+      }
+      delete solution[remainingIngredients[0]];
+      return;
+    } else {
+      remainingIngredients.forEach((ing) => {
+        let newRemaining = remainingIngredients.filter((i) => i !== ing);
+        for (let i = 0; i < limit - total + 1; i++) {
+          solution[ing] = i;
+          backtrack(solution, newRemaining, total + i);
+          delete solution[ing];
+        }
+      });
+    }
+  };
+
+  backtrack({} as Record<string, number>, allIngredients, 0);
+  return max;
+};
+export { parseInput, part1, score1, part2, score2 };
