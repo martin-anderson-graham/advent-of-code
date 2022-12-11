c5ffbdf76172e1061dd838b3f19342c3f3759bd7
Martin Anderson Graham <martin.anderson.graham@gmail.com>
2022-12-11 09:09:19 -0600

    Finished 2022 day11

--- 2022/Day11-Monkey-In-The-Middle/day11.test.ts
+++ 2022/Day11-Monkey-In-The-Middle/day11.test.ts
@@ -1,7 +1,35 @@
-import { parseInput } from "./day11";
+import { parseInput, processRound, processRounds } from "./day11";
 import { sample, input } from "./input";
 
 test("sample", () => {
   const monkeys = parseInput(sample);
-  monkeys.forEach((monkey) => console.log(monkey.items));
+  let res = processRounds(monkeys, 20);
+  expect(res).toBe(10605);
+});
+
+test("part1 parsing check", () => {
+  const monkeys = parseInput(input);
+  let i = { worry: 3 };
+  monkeys.forEach((monkey) => {
+    monkey.operation(i);
+  });
+  expect(i.worry).toBe(42573);
+});
+
+test("part1", () => {
+  const monkeys = parseInput(input);
+  let res = processRounds(monkeys, 20);
+  expect(res).toBe(66124);
+});
+
+test("sample2", () => {
+  const monkeys = parseInput(sample);
+  let res = processRounds(monkeys, 10000, false);
+  expect(res).toBe(2713310158);
+});
+
+test("part2", () => {
+  const monkeys = parseInput(input);
+  let res = processRounds(monkeys, 10000, false);
+  expect(res).toBe(19309892877);
 });
--- 2022/Day11-Monkey-In-The-Middle/day11.ts
+++ 2022/Day11-Monkey-In-The-Middle/day11.ts
@@ -4,11 +4,14 @@ type Item = {
 
 type Monkey = {
   items: Item[];
-  // operation: (arg0: number) => number;
-  // test: (arg0: number) => void;
+  count: number;
+  operation: (arg0: Item) => void;
+  test: (arg0: Item, arg1: Monkey[]) => void;
+  bigMod: number;
 };
 
 const parseInput = (str: string): Monkey[] => {
+  let bigMod = 1;
   let result: Monkey[] = [];
   str.split("\n\n").forEach((block) => {
     let arr = block.split("\n");
@@ -20,10 +23,75 @@ const parseInput = (str: string): Monkey[] => {
           worry: Number(v),
         };
       });
+    let operation: (arg0: Item) => void;
+    let opLine = arr[2].trim().split(" ");
+    if (opLine[4] === "+") {
+      if (opLine[5] === "old") {
+        operation = (old: Item): void => {
+          old.worry += old.worry;
+        };
+      } else {
+        operation = (old: Item): void => {
+          old.worry += Number(opLine[5]);
+        };
+      }
+    } else {
+      if (opLine[5] === "old") {
+        operation = (old: Item): void => {
+          old.worry *= old.worry;
+        };
+      } else {
+        operation = (old: Item): void => {
+          old.worry *= Number(opLine[5]);
+        };
+      }
+    }
+    bigMod *= Number(arr[3].trim().split(" ")[3]);
+    let test = (item: Item, monkeys: Monkey[]) => {
+      let testNum = Number(arr[3].trim().split(" ")[3]);
+      if (item.worry % testNum === 0) {
+        monkeys[Number(arr[4].trim().split(" ")[5])].items.push(item);
+      } else {
+        monkeys[Number(arr[5].trim().split(" ")[5])].items.push(item);
+      }
+    };
 
-    result.push({ items });
+    result.push({ bigMod, items, count: 0, operation, test });
   });
+
+  result.forEach((m) => (m.bigMod = bigMod));
   return result;
 };
 
-export { parseInput };
+const processRound = (monkeys: Monkey[], part1: boolean): void => {
+  monkeys.forEach((monkey) => {
+    while (monkey.items.length > 0) {
+      monkey.count += 1;
+      let current = monkey.items.shift();
+      monkey.operation(current);
+      if (part1) {
+        current.worry = Math.floor(current.worry / 3);
+      } else {
+        current.worry = current.worry % monkey.bigMod;
+      }
+      monkey.test(current, monkeys);
+    }
+  });
+};
+
+const processRounds = (
+  monkeys: Monkey[],
+  rounds: Number,
+  part1 = true
+): number => {
+  for (let i = 0; i < rounds; i++) {
+    processRound(monkeys, part1);
+  }
+  return monkeys
+    .map((m) => m.count)
+    .sort((a, b) => b - a)
+    .slice(0, 2)
+    .reduce((prod, ele) => prod * ele, 1);
+};
+
+export { parseInput, processRound, processRounds };
