08f3f547977139db7b735106b54195adda424a1f
Martin Anderson Graham <martin.anderson.graham@gmail.com>
2022-12-12 06:43:54 -0600

    Update 2022 day12

--- 2022/Day12-Hill-Climbing/day12.test.ts
+++ 2022/Day12-Hill-Climbing/day12.test.ts
@@ -1,9 +1,5 @@
 import { sample, input } from "./input";
-import {
-  parseInput,
-  findMinimumEnergy,
-  findMinimumEnergyAnywhere,
-} from "./day12";
+import { parseInput, findMinimumEnergy } from "./day12";
 
 test("sample", () => {
   let i = parseInput(sample);
@@ -19,12 +15,12 @@ test("part1", () => {
 
 test("sample2", () => {
   let i = parseInput(sample);
-  let res = findMinimumEnergyAnywhere(i);
+  let res = findMinimumEnergy(i, false);
   expect(res).toBe(29);
 });
 
 test("part2", () => {
   let i = parseInput(input);
-  let res = findMinimumEnergyAnywhere(i);
+  let res = findMinimumEnergy(i, false);
   expect(res).toBe(414);
 });
