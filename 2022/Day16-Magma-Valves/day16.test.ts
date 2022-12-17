0e3e294d5bb2f892049e0d647fc91a8cc1d37c15
Martin Anderson Graham <martin.anderson.graham@gmail.com>
2022-12-16 22:51:17 -0600

    Finished 2022 day16 part1

--- 2022/Day16-Magma-Valves/day16.test.ts
+++ 2022/Day16-Magma-Valves/day16.test.ts
@@ -1,8 +1,26 @@
 import { sample, input } from "./input";
-import { parseInput, findBestPath } from "./day16";
+import {
+  parseInput,
+  findBestPath,
+  preComputeMiniPaths,
+  findBestPath2,
+} from "./day16";
 
 test("sample", () => {
   let i = parseInput(sample);
   let res = findBestPath(i);
   expect(res).toBe(1651);
+});
+
+//takes 3 minutes...
+xtest("part1", () => {
+  let i = parseInput(input);
+  let res = findBestPath(i);
+  expect(res).toBe(2183);
+});
+
+test("sample2", () => {
+  let i = parseInput(sample);
+  let res = findBestPath2(i);
+  expect(res).toBe(1707);
 });
--- 2022/Day16-Magma-Valves/day16.ts
+++ 2022/Day16-Magma-Valves/day16.ts
@@ -22,66 +22,246 @@ const parseInput = (str: string): Rooms => {
   }, {});
 };
 
-type Path = {
-  roomsToVisit: Map<string, boolean>;
-  visitedRooms: Record<string, boolean>;
-  total: number;
-  time: number;
-  pressureToAddEachMin: number;
+const preComputeMiniPaths = (
+  roomsToVisit: Map<string, boolean>,
+  rooms: Rooms
+): Record<string, number> => {
+  let result: Record<string, number> = {};
+
+  type RoomNode = {
+    name: string;
+    value: number;
+    neighbors: string[];
+  };
+  const findMinPath = (startRoom: string, endRoom: string): void => {
+    let queue: RoomNode[] = [
+      { name: startRoom, value: 0, neighbors: rooms[startRoom].neighbors },
+    ];
+
+    while (queue.length !== 0) {
+      let current: RoomNode = queue.shift();
+      if (current.name === endRoom) {
+        result[`${startRoom},${endRoom}`] = current.value;
+        return;
+      } else {
+        current.neighbors.forEach((n) => {
+          queue.push({
+            name: n,
+            value: current.value + 1,
+            neighbors: rooms[n].neighbors,
+          });
+        });
+      }
+    }
+  };
+
+  roomsToVisit.forEach((_, key1) => {
+    roomsToVisit.forEach((_, key2) => {
+      if (key1 !== key2) {
+        findMinPath(key1, key2);
+      }
+    });
+  });
+  return result;
 };
 
 const findBestPath = (rooms: Rooms): number => {
+  type Path = {
+    roomsToVisit: Map<string, boolean>;
+    total: number;
+    time: number;
+  };
   let bestTotal = 0;
 
   let roomsToVisit = new Map(
-    Object.keys(rooms).map((k) => {
-      return [k, true];
-    })
+    Object.keys(rooms)
+      .filter((r) => rooms[r].rate !== 0)
+      .map((k) => {
+        return [k, true];
+      })
   );
+  //need 'AA' to be in here even though its rate is zero
+  roomsToVisit.set("AA", true);
+  //keys are 'XX,YY', values are number of steps to get to target room
+  let bestRoomPaths = preComputeMiniPaths(roomsToVisit, rooms);
+  //now we delete it
+  roomsToVisit.delete("AA");
 
   const path: Path = {
     roomsToVisit,
-    visitedRooms: {},
     total: 0,
     time: 30,
-    pressureToAddEachMin: 0,
   };
 
-  const getMiniPath = (currentRoom: string, targetRoom: string): string[] => {
-    return ["aa", "bb"];
-  };
-
-  const walk = (
-    currentRoomName: string,
-    miniPath: string[],
-    pressure: number
-  ): void => {
-    if (path.roomsToVisit.size === 0) {
-      pressure += (30 - path.time) * path.pressureToAddEachMin;
-      if (pressure > bestTotal) {
+  const walk = (currentRoomName: string): void => {
+    //if we hit zero time we are done, or if there are no more rooms to open
+    if (path.time <= 0 || path.roomsToVisit.size === 0) {
+      if (path.total > bestTotal) {
         bestTotal = path.total;
       }
       return;
     }
+    path.roomsToVisit.delete(currentRoomName);
 
-    //generate the first set of targetRooms
-    if (miniPath.length === 0) {
+    let toVisit = Array.from(path.roomsToVisit.keys());
+    toVisit.forEach((r) => {
+      path.time -= bestRoomPaths[`${currentRoomName},${r}`];
+      //turn off the valve
       path.time -= 1;
-      let remainingRooms = path.roomsToVisit.keys();
-      let nextRoomObj = remainingRooms.next();
-      while (!nextRoomObj.done) {
-        let miniPath = getMiniPath(currentRoomName, nextRoomObj.value);
-        miniPath.forEach((room) => {
-          walk(room, miniPath.slice(1), 0);
-        });
-
-        nextRoomObj = remainingRooms.next();
-      }
-    }
+      //add the total released pressure for that room
+      let remainingTime = path.time >= 0 ? path.time : 0;
+      path.total += rooms[r].rate * remainingTime;
+      path.roomsToVisit.delete(r);
+      walk(r);
+      path.roomsToVisit.set(r, true);
+      path.total -= rooms[r].rate * remainingTime;
+      path.time += 1;
+      path.time += bestRoomPaths[`${currentRoomName},${r}`];
+    });
+    path.roomsToVisit.set(currentRoomName, true);
   };
 
-  // walk("AA", 0);
+  walk("AA");
   return bestTotal;
 };
 
-export { parseInput, findBestPath };
+const findBestPath2 = (rooms: Rooms): number => {
+  type Path = {
+    roomsToVisit: Map<string, boolean>;
+    total: number;
+    time: number;
+    yourCurrentRoom: string;
+    yourTargetRoom: string;
+    yourTravelTime: number;
+    eleCurrentRoom: string;
+    eleTargetRoom: string;
+    eleTravelTime: number;
+  };
+  let bestTotal = 0;
+  let roomsToVisit = new Map(
+    Object.keys(rooms)
+      .filter((r) => rooms[r].rate !== 0)
+      .map((k) => {
+        return [k, true];
+      })
+  );
+  //need 'AA' to be in here even though its rate is zero
+  roomsToVisit.set("AA", true);
+  //keys are 'XX,YY', values are number of steps to get to target room
+  let bestRoomPaths = preComputeMiniPaths(roomsToVisit, rooms);
+  //now we delete it
+  roomsToVisit.delete("AA");
+  /*
+   * The plan:
+   * pre-compute best paths between rooms
+   * Backtrack to each node with a rate value not visited
+   * look up the path from that room to target room
+   * Walk that path, then generate the next ()
+   *
+   */
+  const path: Path = {
+    roomsToVisit,
+    total: 0,
+    time: 27,
+    yourCurrentRoom: "AA",
+    yourTargetRoom: "AA",
+    yourTravelTime: 1,
+    eleCurrentRoom: "AA",
+    eleTargetRoom: "AA",
+    eleTravelTime: 1,
+  };
+
+  const walk = (): void => {
+    console.log(path.yourTargetRoom, path.eleTargetRoom);
+    path.time -= 1;
+    path.yourTravelTime -= 1;
+    path.eleTravelTime -= 1;
+    //if we hit zero time we are done, or if there are no more rooms to open
+    if (
+      path.time <= 0 ||
+      (Number.isNaN(path.yourTravelTime) && Number.isNaN(path.eleTravelTime))
+    ) {
+      return;
+    }
+
+    //maybe if you both are stopped who gets the closer one?
+    if (path.yourTravelTime === 0 && path.eleTravelTime === 0) {
+      path.yourCurrentRoom = path.yourTargetRoom;
+      path.eleCurrentRoom = path.eleTargetRoom;
+      let yourBestRoom = "";
+      let yourBestVal = -Infinity;
+      path.roomsToVisit.forEach((_, r) => {
+        let remainingTimeAfterMoving =
+          path.time - bestRoomPaths[`${path.yourCurrentRoom},${r}`] - 1;
+        if (remainingTimeAfterMoving * rooms[r].rate > yourBestVal) {
+          yourBestVal = remainingTimeAfterMoving * rooms[r].rate;
+          yourBestRoom = r;
+        }
+      });
+      let eleBestRoom = "";
+      let eleBestVal = -Infinity;
+      path.roomsToVisit.forEach((_, r) => {
+        let remainingTimeAfterMoving =
+          path.time - bestRoomPaths[`${path.eleCurrentRoom},${r}`] - 1;
+        if (remainingTimeAfterMoving * rooms[r].rate > eleBestVal) {
+          eleBestVal = remainingTimeAfterMoving * rooms[r].rate;
+          eleBestRoom = r;
+        }
+      });
+      if (eleBestVal > yourBestVal) {
+        path.eleCurrentRoom = path.eleTargetRoom;
+        path.total += path.time * rooms[path.eleCurrentRoom].rate;
+        path.roomsToVisit.delete(eleBestRoom);
+        path.eleTargetRoom = eleBestRoom;
+        path.eleTravelTime =
+          bestRoomPaths[`${path.eleCurrentRoom},${path.eleTargetRoom}`] + 1;
+      }
+    }
+
+    //lets try being greedy - human
+    if (path.yourTravelTime === 0) {
+      path.yourCurrentRoom = path.yourTargetRoom;
+      path.total += path.time * rooms[path.yourCurrentRoom].rate;
+      let bestRoom = "";
+      let bestVal = -Infinity;
+      path.roomsToVisit.forEach((_, r) => {
+        let remainingTimeAfterMoving =
+          path.time - bestRoomPaths[`${path.yourCurrentRoom},${r}`] - 1;
+        if (remainingTimeAfterMoving * rooms[r].rate > bestVal) {
+          bestVal = remainingTimeAfterMoving * rooms[r].rate;
+          bestRoom = r;
+        }
+      });
+      path.roomsToVisit.delete(bestRoom);
+      path.yourTargetRoom = bestRoom;
+      path.yourTravelTime =
+        bestRoomPaths[`${path.yourCurrentRoom},${path.yourTargetRoom}`] + 1;
+    }
+
+    //lets try being greedy - ele
+    if (path.eleTravelTime === 0) {
+      path.eleCurrentRoom = path.eleTargetRoom;
+      path.total += path.time * rooms[path.eleCurrentRoom].rate;
+      let bestRoom = "";
+      let bestVal = -Infinity;
+      path.roomsToVisit.forEach((_, r) => {
+        let remainingTimeAfterMoving =
+          path.time - bestRoomPaths[`${path.eleCurrentRoom},${r}`] - 1;
+        if (remainingTimeAfterMoving * rooms[r].rate > bestVal) {
+          bestVal = remainingTimeAfterMoving * rooms[r].rate;
+          bestRoom = r;
+        }
+      });
+      path.roomsToVisit.delete(bestRoom);
+      path.eleTargetRoom = bestRoom;
+      path.eleTravelTime =
+        bestRoomPaths[`${path.eleCurrentRoom},${path.eleTargetRoom}`] + 1;
+    }
+
+    walk();
+  };
+
+  walk();
+  return path.total;
+};
+export { parseInput, findBestPath, preComputeMiniPaths, findBestPath2 };
