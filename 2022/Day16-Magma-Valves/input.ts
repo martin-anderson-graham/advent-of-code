8a114d5aeaba5557bfbfc0105eb445f92d0df2b2
Martin Anderson Graham <martin.anderson.graham@gmail.com>
2022-12-16 22:52:14 -0600

    Quick update

--- 2022/Day16-Magma-Valves/input.ts
+++ 2022/Day16-Magma-Valves/input.ts
@@ -1,3 +1,9 @@
+const sampleOrder = `
+JJ  DD
+BB  HH
+CC  EE
+`;
+
 const sample = `Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
 Valve BB has flow rate=13; tunnels lead to valves CC, AA
 Valve CC has flow rate=2; tunnels lead to valves DD, BB
