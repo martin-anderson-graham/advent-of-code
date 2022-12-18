a0f0a948afc7efdea8968eb4e8966fb4387b110f
Martin Anderson Graham <martin.anderson.graham@gmail.com>
2022-12-17 22:58:11 -0600

    Finished 2022 day17 part1

--- 2022/Day17-Flow/day17.test.ts
+++ 2022/Day17-Flow/day17.test.ts
@@ -4,5 +4,18 @@ import { runSimulation } from "./day17";
 
 test("sample", () => {
   let i = parseInput(rocks, sample);
-  let res = runSimulation(i, 1);
+  let res = runSimulation(i, 2022);
+  expect(res).toBe(3068);
+});
+
+test("part1", () => {
+  let i = parseInput(rocks, input);
+  let res = runSimulation(i, 2022);
+  expect(res).toBe(3161);
+});
+
+test("sample2", () => {
+  // let i = parseInput(rocks, sample);
+  // let res = runSimulation(i, 1000000000000);
+  // expect(res).toBe(1514285714288);
 });
--- 2022/Day17-Flow/day17.ts
+++ 2022/Day17-Flow/day17.ts
@@ -2,12 +2,59 @@ import { Input, printGrid } from "./utils";
 
 const nextMoveIsValid = (
   rock: boolean[][],
-  jet: string,
   grid: boolean[][],
   r: number,
   c: number
 ): boolean => {
-  return r >= 0;
+  if (r < 0) {
+    return false;
+  }
+
+  for (let y = 0; y < rock.length; y++) {
+    for (let x = 0; x < rock[y].length; x++) {
+      if (grid[r + y]?.[c + x] && rock[y][x]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+const jetPush = (
+  grid: boolean[][],
+  r: number,
+  c: number,
+  jets: string[],
+  jetIndex: number,
+  rock: boolean[][]
+): number => {
+  let newC = c;
+
+  let jet = jets[jetIndex];
+  if (jet === ">") {
+    for (let y = 0; y < rock.length; y++) {
+      for (let x = 0; x < rock[y].length; x++) {
+        if (grid[r + y]?.[c + 1 + x] && rock[y][x]) {
+          return newC;
+        }
+      }
+    }
+    if (c + rock[0].length < 7) {
+      newC = c + 1;
+    }
+  } else if (jet === "<") {
+    for (let y = 0; y < rock.length; y++) {
+      for (let x = 0; x < rock[y].length; x++) {
+        if (grid[r + y]?.[c - 1 + x] && rock[y][x]) {
+          return newC;
+        }
+      }
+    }
+    if (c > 0) {
+      newC = c - 1;
+    }
+  }
+  return newC;
 };
 
 const runRound = (
@@ -18,9 +65,9 @@ const runRound = (
 ): number => {
   //add rows until there are three blank ones
   while (
-    grid[grid.length - 1].includes(true) &&
-    grid[grid.length - 2].includes(true) &&
-    grid[grid.length - 3].includes(true) &&
+    grid[grid.length - 1].includes(true) ||
+    grid[grid.length - 2].includes(true) ||
+    grid[grid.length - 3].includes(true) ||
     grid[grid.length - 4].includes(true)
   ) {
     grid.push(new Array(7).fill(false));
@@ -30,19 +77,9 @@ const runRound = (
   //going to try not having the whole rock there at once
   let jetIndex = startingJetIndex;
   while (true) {
-    let jet = jets[jetIndex];
-    console.log(r, c, jet);
-    if (jet === ">") {
-      if (c + rock[0].length < 7) {
-        c++;
-      }
-    } else if (jet === "<") {
-      if (c > 0) {
-        c--;
-      }
-    }
+    c = jetPush(grid, r, c, jets, jetIndex, rock);
     jetIndex = (jetIndex + 1) % jets.length;
-    if (!nextMoveIsValid(rock, jet, grid, r - 1, c)) {
+    if (!nextMoveIsValid(rock, grid, r - 1, c)) {
       break;
     }
     r--;
@@ -55,7 +92,7 @@ const runRound = (
   return jetIndex;
 };
 
-const runSimulation = (input: Input, rounds: number): void => {
+const runSimulation = (input: Input, rounds: number): number => {
   let rockIndex = 0;
   let jetIndex = 0;
   for (let i = 0; i < rounds; i++) {
@@ -66,9 +103,13 @@ const runSimulation = (input: Input, rounds: number): void => {
       input.grid
     );
     rockIndex = (rockIndex + 1) % 5;
-
-    printGrid(input.grid);
   }
+  for (let i = 0; i < input.grid.length; i++) {
+    if (!input.grid[i].includes(true)) {
+      return i;
+    }
+  }
+  return -1;
 };
 
 export { runSimulation };
