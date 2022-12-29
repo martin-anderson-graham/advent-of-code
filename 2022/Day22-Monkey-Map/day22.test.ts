a0e3d096af2cbf1697777de148e90840dc52ee46
Martin Anderson Graham <martin.anderson.graham@gmail.com>
2022-12-29 11:22:45 -0700

    Finished 2022 day22

--- 2022/Day22-Monkey-Map/day22.test.ts
+++ 2022/Day22-Monkey-Map/day22.test.ts
@@ -1,5 +1,21 @@
-import { sample, input } from "./input"
+import { sample, input, sampleEdgeMappings, sampleSideMappings, inputEdgeMappings, inputSideMappings } from "./input"
 import { parseInput } from "./day22"
+
+test("sidemappings", () => {
+    let grid:string[][] = []
+    for(let r = 0; r < 12; r++) {
+        grid.push(new Array(12).fill(' '))
+    }
+    for(let i = 1; i <= 6; i++) {
+        let side = sampleSideMappings[i]
+        for (let r = side.startRow; r<=side.endRow; r++) {
+            for (let c = side.startCol; c <= side.endCol; c++) {
+                grid[r][c] = String(i)
+            }
+        }
+    }
+    // console.log(grid.map(l => l.join('')).join('\n'))
+})
 
 test("basics", () => {
     let [board, ints] = parseInput(sample)
@@ -40,6 +56,193 @@ test("basics", () => {
     expect(nboard.player.direction).toBe('>')
 })
 
+test("cube wrappings", () => {
+    let [board, ints] = parseInput(input, inputEdgeMappings, inputSideMappings)
+    //from 1 to 6
+    board.player.row = 0
+    board.player.col = 50
+    board.player.direction = '^'
+    let nextPosition = board.player.determineNextCubePosition([0,50], board.grid)
+    expect(nextPosition).toEqual([[150, 0], '>'])
+    //from 6 to 1
+    board.player.row =150 
+    board.player.col = 0
+    board.player.currentSide = board.player.determineCurrentSide()
+    board.player.direction = '<'
+    nextPosition = board.player.determineNextCubePosition([board.player.row, board.player.col], board.grid)
+    expect(nextPosition).toEqual([[0, 50], 'v'])
+    //from 1 to 2
+    board.player.row = 0
+    board.player.col = 99 
+    board.player.currentSide = board.player.determineCurrentSide()
+    board.player.direction = '>'
+    nextPosition = board.player.determineNextCubePosition([board.player.row, board.player.col], board.grid)
+    expect(nextPosition).toEqual([[0, 100], '>'])
+    //from 2 to 1
+    board.player.row = 0 
+    board.player.col = 100 
+    board.player.currentSide = board.player.determineCurrentSide()
+    board.player.direction = '<'
+    nextPosition = board.player.determineNextCubePosition([board.player.row, board.player.col], board.grid)
+    expect(nextPosition).toEqual([[0, 99], '<'])
+    //from 1 to 4
+    board.player.row = 0 
+    board.player.col = 50 
+    board.player.currentSide = board.player.determineCurrentSide()
+    board.player.direction = '<'
+    nextPosition = board.player.determineNextCubePosition([board.player.row, board.player.col], board.grid)
+    expect(nextPosition).toEqual([[149, 0], '>'])
+    //from 4 to 1
+    board.player.row = 149 
+    board.player.col = 0 
+    board.player.currentSide = board.player.determineCurrentSide()
+    board.player.direction = '<'
+    nextPosition = board.player.determineNextCubePosition([board.player.row, board.player.col], board.grid)
+    expect(nextPosition).toEqual([[0, 50], '>'])
+    //from 1 to 3
+    board.player.row = 49 
+    board.player.col = 50 
+    board.player.currentSide = board.player.determineCurrentSide()
+    board.player.direction = 'v'
+    nextPosition = board.player.determineNextCubePosition([board.player.row, board.player.col], board.grid)
+    expect(nextPosition).toEqual([[50, 50], 'v'])
+    //from 3 to 1
+    board.player.row = 50 
+    board.player.col = 50 
+    board.player.currentSide = board.player.determineCurrentSide()
+    board.player.direction = '^'
+    nextPosition = board.player.determineNextCubePosition([board.player.row, board.player.col], board.grid)
+    expect(nextPosition).toEqual([[49, 50], '^'])
+    //from 2 to 3
+    board.player.row = 49 
+    board.player.col = 100 
+    board.player.currentSide = board.player.determineCurrentSide()
+    expect(board.player.currentSide).toBe(2)
+    board.player.direction = 'v'
+    nextPosition = board.player.determineNextCubePosition([board.player.row, board.player.col], board.grid)
+    expect(nextPosition).toEqual([[50, 99], '<'])
+    //from 3 to 2
+    board.player.row = 50 
+    board.player.col = 99 
+    board.player.currentSide = board.player.determineCurrentSide()
+    expect(board.player.currentSide).toBe(3)
+    board.player.direction = '>'
+    nextPosition = board.player.determineNextCubePosition([board.player.row, board.player.col], board.grid)
+    expect(nextPosition).toEqual([[49, 100], '^'])
+    //from 2 to 5
+    board.player.row = 0 
+    board.player.col = 149 
+    board.player.currentSide = board.player.determineCurrentSide()
+    expect(board.player.currentSide).toBe(2)
+    board.player.direction = '>'
+    nextPosition = board.player.determineNextCubePosition([board.player.row, board.player.col], board.grid)
+    expect(nextPosition).toEqual([[149, 99], '<'])
+    //from 5 to 2
+    board.player.row = 149 
+    board.player.col = 99 
+    board.player.currentSide = board.player.determineCurrentSide()
+    expect(board.player.currentSide).toBe(5)
+    board.player.direction = '>'
+    nextPosition = board.player.determineNextCubePosition([board.player.row, board.player.col], board.grid)
+    expect(nextPosition).toEqual([[0, 149], '<'])
+    //from 2 to 6
+    board.player.row = 0 
+    board.player.col = 149 
+    board.player.currentSide = board.player.determineCurrentSide()
+    expect(board.player.currentSide).toBe(2)
+    board.player.direction = '^'
+    nextPosition = board.player.determineNextCubePosition([board.player.row, board.player.col], board.grid)
+    expect(nextPosition).toEqual([[199, 49], '^'])
+    //from 6 to 2
+    board.player.row = 199 
+    board.player.col = 49 
+    board.player.currentSide = board.player.determineCurrentSide()
+    expect(board.player.currentSide).toBe(6)
+    board.player.direction = 'v'
+    nextPosition = board.player.determineNextCubePosition([board.player.row, board.player.col], board.grid)
+    expect(nextPosition).toEqual([[0, 149], 'v'])
+    //from 3 to 4
+    board.player.row = 50 
+    board.player.col = 50 
+    board.player.currentSide = board.player.determineCurrentSide()
+    expect(board.player.currentSide).toBe(3)
+    board.player.direction = '<'
+    nextPosition = board.player.determineNextCubePosition([board.player.row, board.player.col], board.grid)
+    expect(nextPosition).toEqual([[100, 0], 'v'])
+    //from 4 to 3
+    board.player.row = 100 
+    board.player.col = 0 
+    board.player.currentSide = board.player.determineCurrentSide()
+    expect(board.player.currentSide).toBe(4)
+    board.player.direction = '^'
+    nextPosition = board.player.determineNextCubePosition([board.player.row, board.player.col], board.grid)
+    expect(nextPosition).toEqual([[50, 50], '>'])
+    //from 3 to 5
+    board.player.row = 99 
+    board.player.col = 50 
+    board.player.currentSide = board.player.determineCurrentSide()
+    expect(board.player.currentSide).toBe(3)
+    board.player.direction = 'v'
+    nextPosition = board.player.determineNextCubePosition([board.player.row, board.player.col], board.grid)
+    expect(nextPosition).toEqual([[100, 50], 'v'])
+    //from 5 to 3
+    board.player.row = 100 
+    board.player.col = 50 
+    board.player.currentSide = board.player.determineCurrentSide()
+    expect(board.player.currentSide).toBe(5)
+    board.player.direction = '^'
+    nextPosition = board.player.determineNextCubePosition([board.player.row, board.player.col], board.grid)
+    expect(nextPosition).toEqual([[99, 50], '^'])
+    //from 4 to 5
+    board.player.row = 100 
+    board.player.col = 49 
+    board.player.currentSide = board.player.determineCurrentSide()
+    expect(board.player.currentSide).toBe(4)
+    board.player.direction = '>'
+    nextPosition = board.player.determineNextCubePosition([board.player.row, board.player.col], board.grid)
+    expect(nextPosition).toEqual([[100, 50], '>'])
+    //from 5 to 4
+    board.player.row = 100 
+    board.player.col = 50 
+    board.player.currentSide = board.player.determineCurrentSide()
+    expect(board.player.currentSide).toBe(5)
+    board.player.direction = '<'
+    nextPosition = board.player.determineNextCubePosition([board.player.row, board.player.col], board.grid)
+    expect(nextPosition).toEqual([[100, 49], '<'])
+    //from 4 to 6
+    board.player.row = 149 
+    board.player.col = 49 
+    board.player.currentSide = board.player.determineCurrentSide()
+    expect(board.player.currentSide).toBe(4)
+    board.player.direction = 'v'
+    nextPosition = board.player.determineNextCubePosition([board.player.row, board.player.col], board.grid)
+    expect(nextPosition).toEqual([[150, 49], 'v'])
+    //from 6 to 4
+    board.player.row = 150 
+    board.player.col = 49 
+    board.player.currentSide = board.player.determineCurrentSide()
+    expect(board.player.currentSide).toBe(6)
+    board.player.direction = '^'
+    nextPosition = board.player.determineNextCubePosition([board.player.row, board.player.col], board.grid)
+    expect(nextPosition).toEqual([[149, 49], '^'])
+    //from 5 to 6
+    board.player.row = 149 
+    board.player.col = 50 
+    board.player.currentSide = board.player.determineCurrentSide()
+    expect(board.player.currentSide).toBe(5)
+    board.player.direction = 'v'
+    nextPosition = board.player.determineNextCubePosition([board.player.row, board.player.col], board.grid)
+    expect(nextPosition).toEqual([[150, 49], '<'])
+    //from 6 to 5
+    board.player.row = 150 
+    board.player.col = 49 
+    board.player.currentSide = board.player.determineCurrentSide()
+    expect(board.player.currentSide).toBe(6)
+    board.player.direction = '>'
+    nextPosition = board.player.determineNextCubePosition([board.player.row, board.player.col], board.grid)
+    expect(nextPosition).toEqual([[149, 50], '^'])
+})
+
 test("sample", () => {
     let [board, ints] = parseInput(sample)
     board.processInstructions(ints)
@@ -52,4 +255,19 @@ test("part1", () => {
     board.processInstructions(ints)
     let res = board.score()
     expect(res).toBe(27492)
+})
+
+test("sample2", () => {
+    let [board, ints] = parseInput(sample, sampleEdgeMappings, sampleSideMappings)
+    board.processInstructionsCube(ints)
+    let res = board.score()
+    expect(res).toBe(5031)
+})
+
+test("part2", () => {
+    let [board, ints] = parseInput(input, inputEdgeMappings, inputSideMappings)
+    board.processInstructionsCube(ints)
+    let res = board.score()
+    //162052 too high
+    expect(res).toBe(78291)
 })
--- 2022/Day22-Monkey-Map/day22.ts
+++ 2022/Day22-Monkey-Map/day22.ts
@@ -1,3 +1,4 @@
+import { EdgeMapping, SideMapping } from "./input"
 type Direction = '^' | '>' | 'v' | '<'
 
 
@@ -6,12 +7,25 @@ class Player {
     col: number;
     direction: Direction
     directions: Direction[]
+    edgeMappings: Record<string, EdgeMapping>
+    sideMappings: Record<string, SideMapping>
+    currentSide: number;
+    changeArrMapping: Record<Direction, number[]>
 
-    constructor(x: number, y: number) {
+    constructor(x: number, y: number, edgeMappings: Record<string, EdgeMapping>, sideMappings: Record<string, SideMapping>) {
         this.row = x
         this.col = y
         this.direction = '>'
         this.directions = ['^', '>', 'v', '<']
+        this.edgeMappings = edgeMappings
+        this.sideMappings = sideMappings
+        this.currentSide = 1
+        this.changeArrMapping = {
+            '>': [0, 1],
+            'v': [1, 0],
+            '<': [0, -1],
+            '^': [-1, 0],
+        }
     }
 
     turnLeft() {
@@ -49,6 +63,102 @@ class Player {
         return nextPosition
     }
 
+    determineNextCubePosition(priorPosition: number[], grid: string[][]): [number[], Direction] {
+        let changeArr: number[] = this.changeArrMapping[this.direction]
+        let newDirection = this.direction
+        let side = this.sideMappings[this.currentSide]
+        let nextPosition = [priorPosition[0] + changeArr[0], priorPosition[1] + changeArr[1]]
+        //possibilites
+        //1. no wrap (might change side or not, no change in direction)
+        //2. new side, wrap, new direction
+        //  a. wrap off of top/right/bottom/left
+
+        //same side or new side without wrapping
+        if (nextPosition[0] >= 0 && nextPosition[0] < grid.length &&
+            nextPosition[1] >= 0 && nextPosition[1] < grid[0].length &&
+            grid[nextPosition[0]][nextPosition[1]] !== ' ') {
+            return [nextPosition, newDirection]
+        }
+        //need to wrap
+        let transition = this.edgeMappings[`${this.currentSide}${this.direction}`]
+        newDirection = transition.newDir
+        let newSide = this.sideMappings[transition.newSide]
+        if (nextPosition[0] < side.startRow) { //wrap top
+            //compute the mapping of columns and rows based on the change of directions 
+            let currentRelativeCol = priorPosition[1] - side.startCol
+            if (newDirection === 'v') {
+                let nextRelativeCol = newSide.endCol - newSide.startCol - currentRelativeCol
+                nextPosition[0] = newSide.startRow
+                nextPosition[1] = nextRelativeCol + newSide.startCol
+            } else if (newDirection === '<') {
+                let nextRelativeRow = newSide.endRow - newSide.startRow - currentRelativeCol
+                nextPosition[0] = nextRelativeRow + newSide.startRow
+                nextPosition[1] = newSide.endCol
+            } else if (newDirection === '>') {
+                let nextRelativeRow = currentRelativeCol
+                nextPosition[0] = nextRelativeRow + newSide.startRow
+                nextPosition[1] = newSide.startCol
+            } else if (newDirection === '^') {
+                nextPosition[0] = newSide.endRow
+                nextPosition[1] = currentRelativeCol + newSide.startCol
+            }
+        } else if (nextPosition[0] > side.endRow) { //wrap bottom so direction = 'v'
+            let currentRelativeCol = priorPosition[1] - side.startCol
+            if (newDirection === '^') {
+                let nextRelativeCol = newSide.endCol - newSide.startCol - currentRelativeCol
+                nextPosition[0] = newSide.endRow
+                nextPosition[1] = nextRelativeCol + newSide.startCol
+            } else if (newDirection === '>') {
+                let nextRelativeRow = newSide.endRow - newSide.startRow - currentRelativeCol
+                nextPosition[0] = nextRelativeRow + newSide.startRow
+                nextPosition[1] = newSide.startCol
+            } else if (newDirection === '<') {
+                let nextRelativeRow = currentRelativeCol
+                nextPosition[0] = nextRelativeRow + newSide.startRow
+                nextPosition[1] = newSide.endCol
+            } else if (newDirection === 'v') {
+                nextPosition[0] = newSide.startRow
+                nextPosition[1] = currentRelativeCol + newSide.startCol
+            }
+        } else if (nextPosition[1] < side.startCol) { //wrap left so direction = '<'
+            let currentRelativeRow = priorPosition[0] - side.startRow
+            if (newDirection === 'v') {
+                let nextRelativeCol = currentRelativeRow
+                nextPosition[0] = newSide.startRow
+                nextPosition[1] = nextRelativeCol + newSide.startCol
+            } else if (newDirection === '^') {
+                let nextRelativeCol = newSide.endCol - newSide.startCol - currentRelativeRow
+                nextPosition[0] = newSide.endRow
+                nextPosition[1] = nextRelativeCol + newSide.startCol
+            } else if (newDirection === '>') {
+                let nextRelativeRow = newSide.endRow - newSide.startRow - currentRelativeRow
+                nextPosition[0] = nextRelativeRow + newSide.startRow
+                nextPosition[1] = newSide.startCol
+            } else if (newDirection === '<') {
+                nextPosition[0] = currentRelativeRow + newSide.startRow
+                nextPosition[1] = newSide.endCol
+            }
+        } else if (nextPosition[1] > side.endCol) { //wrap right so direction = '>'
+            let currentRelativeRow = priorPosition[0] - side.startRow
+            if (newDirection === 'v') {
+                let nextRelativeCol = newSide.endCol - newSide.startCol - currentRelativeRow
+                nextPosition[0] = newSide.startRow
+                nextPosition[1] = nextRelativeCol + newSide.startCol
+            } else if (newDirection === '<') {
+                let nextRelativeRow = newSide.endRow - newSide.startRow - currentRelativeRow
+                nextPosition[0] = nextRelativeRow + newSide.startRow
+                nextPosition[1] = newSide.endCol
+            } else if (newDirection === '^') {
+                let nextRelativeCol = currentRelativeRow
+                nextPosition[0] = newSide.endRow
+                nextPosition[1] = newSide.startCol + nextRelativeCol
+            } else if (newDirection === '>') {
+                nextPosition[0] = currentRelativeRow + newSide.startRow
+                nextPosition[1] = newSide.startCol
+            }
+        }
+        return [nextPosition, newDirection]
+    }
     wrapPosition(nextPosition: number[], grid: string[][], changeArr: number[]): number[] {
         let tempChangeArr: number[] = [changeArr[0] * -1, changeArr[1] * -1]
         nextPosition = this.determineNextPosition(nextPosition, grid, tempChangeArr)
@@ -61,16 +171,7 @@ class Player {
     }
 
     move(steps: number, grid: string[][]) {
-
-        //assumes right
-        let changeArr: number[] = [0, 1]
-        if (this.direction === '<') {
-            changeArr = [0, -1]
-        } else if (this.direction === 'v') {
-            changeArr = [1, 0]
-        } else if (this.direction === '^') {
-            changeArr = [-1, 0]
-        }
+        let changeArr: number[] = this.changeArrMapping[this.direction]
 
         let nextPosition = this.determineNextPosition([this.row, this.col], grid, changeArr)
         for (let i = 0; i < steps; i++) {
@@ -89,23 +190,64 @@ class Player {
         }
     }
 
+    determineCurrentSide() {
+        return this.determineSide([this.row, this.col])
+    }
+
+    determineSide(position: number[]): number {
+        for (let i = 1; i <= 6; i++) {
+            let side = this.sideMappings[String(i)]
+            if (position[0] >= side.startRow && position[0] <= side.endRow) {
+                if (position[1] >= side.startCol && position[1] <= side.endCol) {
+                    return i
+                }
+            }
+        }
+        console.log(this.sideMappings)
+        console.log(position, this.direction)
+        throw new Error("You must be on a side")
+    }
+
+    moveCube(steps: number, grid: string[][]) {
+        this.currentSide = this.determineCurrentSide();
+        let [nextPosition, nextDirection] = this.determineNextCubePosition([this.row, this.col], grid);
+        for (let i = 0; i < steps; i++) {
+            let nextCharacter = grid[nextPosition[0]][nextPosition[1]]
+            // if (nextCharacter === ' ') {
+            //     [nextPosition, nextDirection] = this.determineNextCubePosition([this.row, this.col], grid)
+            //     nextCharacter = grid[nextPosition[0]][nextPosition[1]]
+            // }
+            if (nextCharacter === '#') {
+                return
+            }
+
+            this.row = nextPosition[0]
+            this.col = nextPosition[1]
+            this.direction = nextDirection;
+            this.currentSide = this.determineCurrentSide();
+            [nextPosition, nextDirection] = this.determineNextCubePosition(nextPosition, grid)
+            grid[this.row][this.col] = this.direction
+        }
+    }
 }
 
 class Board {
     grid: string[][];
     player: Player;
+    edgeMappings: Record<string, EdgeMapping>
+    sideMappings: Record<string, SideMapping>
 
-    constructor(inputStr: string) {
+    constructor(inputStr: string, edgeMappings: Record<string, EdgeMapping> = {}, sideMappings: Record<string, SideMapping> = {}) {
         this.grid = []
         let maxLength = 0
         inputStr.split('\n').forEach(line => {
-            if(line.length > maxLength) {
+            if (line.length > maxLength) {
                 maxLength = line.length
             }
             this.grid.push(line.split(''))
         })
         this.grid.forEach((row) => {
-            while(row.length < maxLength) {
+            while (row.length < maxLength) {
                 row.push(' ')
             }
         })
@@ -117,7 +259,10 @@ class Board {
                 break
             }
         }
-        this.player = new Player(0, col)
+        this.edgeMappings = edgeMappings
+        this.sideMappings = sideMappings
+        this.player = new Player(0, col, this.edgeMappings, this.sideMappings)
+
     }
 
     printGrid() {
@@ -126,19 +271,54 @@ class Board {
         console.log(str.join('\n'))
     }
 
+    printSide(side: number) {
+        let str: string[] = []
+        let s = this.sideMappings[side]
+        for (let r = s.startRow; r <= s.endRow; r++) {
+            let line = ''
+            for (let c = s.startCol; c <= s.endCol; c++) {
+                line += this.grid[r][c]
+            }
+            str.push(line)
+        }
+        console.log(str.join('\n'))
+    }
+
     processInstructions(ints: (string | number)[]): void {
         ints.forEach(int => {
             if (int === 'L') {
                 this.player.turnLeft()
-            this.grid[this.player.row][this.player.col] = this.player.direction
+                this.grid[this.player.row][this.player.col] = this.player.direction
             } else if (int === 'R') {
                 this.player.turnRight()
-            this.grid[this.player.row][this.player.col] = this.player.direction
+                this.grid[this.player.row][this.player.col] = this.player.direction
             } else if (typeof int === 'number') {
                 this.player.move(int, this.grid)
             }
         })
     }
+
+    processInstructionsCube(ints: (string | number)[]): void {
+        ints.forEach(int => {
+            if (int === 'L') {
+                this.player.turnLeft()
+                this.grid[this.player.row][this.player.col] = this.player.direction
+            } else if (int === 'R') {
+                this.player.turnRight()
+                this.grid[this.player.row][this.player.col] = this.player.direction
+            } else if (typeof int === 'number') {
+                let toPrint = {
+                    row: this.player.row,
+                    col: this.player.col,
+                    direction: this.player.direction,
+                    currentSide: this.player.currentSide
+                }
+                // console.log(int,toPrint)
+                this.player.moveCube(int, this.grid)
+            }
+        })
+    }
+
     score() {
         let dir = 0
         if (this.player.direction === '>') {
@@ -154,9 +334,9 @@ class Board {
     }
 }
 
-const parseInput = (str: string): [Board, (string | number)[]] => {
+const parseInput = (str: string, edgeMappings: Record<string, EdgeMapping> = {}, sideMappings: Record<string, SideMapping> = {}): [Board, (string | number)[]] => {
     let [boardString, instructionsString] = str.split('\n\n')
-    let b = new Board(boardString)
+    let b = new Board(boardString, edgeMappings, sideMappings)
     let instructions: (string | number)[] = []
 
     let currentInt = ''
