c7bb06ff8457dee53ddbb51d7eabe6d4c8dc9a29
Martin Anderson Graham <martin.anderson.graham@gmail.com>
2024-11-24 08:58:27 -0600

    WIP 2023 day19

--- 2023/day19/src/main.rs
+++ 2023/day19/src/main.rs
@@ -14,9 +14,15 @@ fn part_one(raw: &str) -> usize {
     machine.process()
 }
 
+fn part_two(raw: &str) -> usize {
+    let machine = Machine::from_str(raw).unwrap();
+    let result = 0;
+    result
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::part_one;
+    use crate::{part_one, part_two};
 
     #[test]
     fn part_one_ex1() {
@@ -28,5 +34,11 @@ mod tests {
     fn part_one_input() {
         let input = include_str!("input.txt");
         assert_eq!(part_one(input), 386787);
+    }
+
+    #[test]
+    fn part_two_ex1() {
+        let input = include_str!("ex1.txt");
+        assert_eq!(part_two(input), 167409079868000);
     }
 }
