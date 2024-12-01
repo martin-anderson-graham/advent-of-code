1aed2aba2eafdc682b4378b469104878706b0b4b
Martin Anderson Graham <martin.anderson.graham@gmail.com>
2024-11-30 22:17:56 -0700

    2024 day 01

--- 2024/src/day01/day01.rs
+++ 2024/src/day01/day01.rs
@@ -1,19 +1,76 @@
+use std::collections::HashMap;
+
 use common::PuzzleParts;
 
-pub struct Day01;
+pub struct Day01 {
+    list_one: Vec<isize>,
+    list_two: Vec<isize>,
+    hash_two: HashMap<isize, isize>,
+}
 
 impl Day01 {
     pub fn new(input: &String) -> Self {
-        todo!()
+        let mut list_one = vec![];
+        let mut list_two = vec![];
+
+        input.trim().lines().for_each(|line| {
+            let mut items = line.split_whitespace();
+
+            list_one.push(
+                items
+                    .next()
+                    .expect("Didn't find the first item")
+                    .parse::<isize>()
+                    .expect("Found a non-numerical item"),
+            );
+            list_two.push(
+                items
+                    .next()
+                    .expect("Didn't find the second item")
+                    .parse::<isize>()
+                    .expect("Found a non-numerical item"),
+            );
+        });
+
+        let mut hash_two = HashMap::new();
+        list_two.iter().for_each(|val| {
+            match hash_two.get(val) {
+                Some(count) => hash_two.insert(*val, count + 1),
+                None => hash_two.insert(*val, 1),
+            };
+        });
+
+        Self {
+            list_one,
+            list_two,
+            hash_two,
+        }
     }
 }
 
 impl PuzzleParts for Day01 {
     fn part_one(&mut self) -> String {
-        todo!()
+        let mut sum = 0;
+
+        self.list_one.sort();
+        self.list_two.sort();
+
+        for i in 0..self.list_one.len() {
+            sum += (self.list_two[i] - self.list_one[i]).abs()
+        }
+
+        sum.to_string()
     }
 
     fn part_two(&mut self) -> Option<String> {
-    None
+        let mut total = 0;
+        self.list_one.iter().for_each(|val| {
+            match self.hash_two.get(val){
+                Some(count) => total += *val * count,
+                None => (),
+            };
+        });
+
+            Some(total.to_string())
     }
 }
--- 2024/src/day01/mod.rs
+++ 2024/src/day01/mod.rs
@@ -1,1 +1,36 @@
 pub mod day01;
+
+#[cfg(test)]
+mod tests {
+    use common::PuzzleParts;
+
+    use crate::day01::day01::Day01;
+
+    #[test]
+    fn part_1() {
+        let input = String::from(
+            "3   4
+4   3
+2   5
+1   3
+3   9
+3   3",
+        );
+        let mut puzz = Day01::new(&input);
+        assert_eq!(puzz.part_one(), "11");
+    }
+
+    #[test]
+    fn part_2() {
+        let input = String::from(
+            "3   4
+4   3
+2   5
+1   3
+3   9
+3   3",
+        );
+        let mut puzz = Day01::new(&input);
+        assert_eq!(puzz.part_two(), Some(String::from("31")));
+    }
+}
